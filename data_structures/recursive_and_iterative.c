e2173157fa6162598430a174b957f6b9ad15c3b8
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2024-09-30 22:16:34 +0300

    Update recursive_and_iterative.c

--- data_structures/recursive_and_iterative.c
+++ data_structures/recursive_and_iterative.c
@@ -281,3 +281,53 @@ void sort(int a[],int b,int c){
     }
     
 }
+
+
+// binary search
+#include <stdio.h>
+
+int binary_search(int list[], int searchnum, int left, int right);
+int compare(int a, int b);
+
+int main()
+{
+	int list[5] = { 1,2,3,4,5 };
+	printf("please enter a number to search");
+	int a;
+	scanf_s("%d", &a);
+	int sonuç = binary_search(list,a,0,4);
+	printf("%d", sonuç);
+
+	return 0;
+}
+int binary_search(int list[], int searchnum, int left, int right) {
+	int middle;
+	while (left <= right) {
+		middle = (left + right) / 2;
+		switch (compare(list[middle], searchnum))
+		{
+		case -1: left = middle + 1;
+			break;
+		case 0:
+			return middle;
+			
+		case 1: right = middle - 1;
+			break;
+		}
+	}
+	return -1;
+}
+int compare(int a, int b) {
+	if (a>b)
+	{
+		return 1;
+	}
+	if (a==b)
+	{
+		return 0;
+	}
+	if (b>a)
+	{
+		return -1;
+	}
+}
