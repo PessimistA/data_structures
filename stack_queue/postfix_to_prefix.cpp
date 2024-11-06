0f739699f07fd569db202c5604de27b859706d35
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2024-11-06 12:08:15 +0300

    Update postfix_to_prefix.cpp
    hatalar giderildi

--- stack_queue/postfix_to_prefix.cpp
+++ stack_queue/postfix_to_prefix.cpp
@@ -1,5 +1,4 @@
-//postfix to prefix
-#define _CRT_SECURE_NO_WARNINGS//visual studio kullanıyorum 
+#define _CRT_SECURE_NO_WARNINGS // Visual Studio kullanıyorsanız bu satır gereklidir.
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -10,23 +9,20 @@
 // Yığın için bir yapı
 char stack[MAX][MAX];
 int top = -1;
-char* op1;
-char* op2;
 
 // Yığına veri ekleme fonksiyonu
 void push(char value[]) {
     if (top < (MAX - 1)) {
         top++;
         strcpy(stack[top], value);
-    }
-    else {
+    } else {
         printf("Stack Overflow\n");
     }
 }
 
 // Yığından veri çekme fonksiyonu
 void pop(char op[]) {
-    if (top >=0) {
+    if (top >= 0) {
         strcpy(op, stack[top]);
         top--;
     }
@@ -34,38 +30,38 @@ void pop(char op[]) {
 
 // Operand kontrol fonksiyonu
 int isOperand(char ch) {
-    return isalpha(ch) || isdigit(ch);
+    return isalpha(ch) || isdigit(ch); // Operand: harf ya da rakam
 }
 
 // Postfix'ten Prefix'e dönüşüm fonksiyonu
 void postfixToPrefix(char* postfix, char* prefix) {
     int len = strlen(postfix);
-    int j = 0;
-
+    
     // Postfix ifadesini sağdan sola doğru okuma
-    for (int i = 0; i <len; i++) {
+    for (int i = 0; i < len; i++) {
         char ch = postfix[i];
 
-        if (isalnum(ch)) {
+        if (isOperand(ch)) {
+            // Operand: Yığına ekle
             char operand[2] = { ch, '\0' };
             push(operand);
-        }
-        else {
+        } else {
             // Operatör: Yığına iki operand al
-            pop(op1);
-            pop(op2);
+            char op1[MAX], op2[MAX];
+            pop(op1); // Birinci operand
+            pop(op2); // İkinci operand
 
             // Operatörü ve iki operand'ı Prefix formatında birleştir
-            char newexp[100];
-            newexp[0] = ch;
+            char newexp[MAX];
+            newexp[0] = ch; // Operatörü ekle
             newexp[1] = '\0';
-            strcat(newexp, op2);
-            strcat(newexp, op1);
-            push(newexp);
+            strcat(newexp, op2); // İkinci operand
+            strcat(newexp, op1); // Birinci operand
+            push(newexp); // Yeni ifadenin yığına eklenmesi
         }
     }
 
-    // Prefix ifadesini almak
+    // Prefix ifadesini almak (yığındaki son eleman)
     strcpy(prefix, stack[top]);
 }
 
