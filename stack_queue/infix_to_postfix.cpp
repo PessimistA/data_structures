c61b234d67bd6c5b7309314f7311eaab6f3f0a5b
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2024-11-08 16:42:02 +0300

    Update infix_to_postfix.cpp

--- stack_queue/infix_to_postfix.cpp
+++ stack_queue/infix_to_postfix.cpp
@@ -88,3 +88,89 @@ int main() {
 
     return 0;
 }
+//ver2
+#define _CRT_SECURE_NO_WARNINGS // Visual Studio kullanıyorsanız bu satır gereklidir.
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+char stack[100];
+int top = -1;
+void push(char value) {
+	if (top < (MAX - 1)) {
+		stack[++top] = value;
+
+	}
+}
+
+char pop() {
+	if (top >= 0) {
+		return stack[top--];
+	}
+	else {
+		printf("Stack Underflow\n");
+		return -1;
+	}
+}
+
+int precedence(char ch) {//öncelik kontrolü
+	switch (ch)
+	{
+	case '+':
+	case '-':
+		return 1;
+	case '*':
+	case '/':
+		return 2;
+	default:
+		return -1;
+	}
+}
+int isoperand(char ch) {
+	return isalpha(ch) || isdigit(ch);
+}
+int isOperator(char ch) {
+	return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+}
+void infix_to_postfix(char infix[], char postfix[]) {
+	int len = strlen(infix);
+	int j = 0;
+	for (int i = 0; i < len; i++)
+	{
+		int ch = infix[i];
+		if (isoperand(ch))
+		{
+			postfix[j++] = ch;
+		}
+
+		else if (ch == '(')//parantez açılımı
+		{
+			push(ch);
+		}
+		else if (ch == ')') {//parantez kapatılması
+			while (top != -1 && stack[top] != '(') {//parantez kapanınca aradakiler dömüşür
+				postfix[j++] = pop();
+			}
+			pop();
+		}
+		else if (isOperator(ch)) {
+			while (top != -1 && precedence(stack[top]) >= precedence(ch)) {
+				postfix[j++] = pop();
+			}
+			push(ch);
+		}
+	}
+	while (top != -1) {
+		postfix[j++] = pop();
+	}
+	postfix[j] = '\0';
+}
+int main() {
+	char infix[MAX] = "A+B*(C-D)";//alınmazsa yukarıdakini açarak kendi değerinizi girebilirsiniz
+	char postfix[100];
+	infix_to_postfix(infix,postfix);
+	printf("son hali:%s", postfix);
+	return 0;
+}
