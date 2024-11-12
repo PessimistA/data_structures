ed67ba774da6ae04da58af3b36c779ebf83c8bf7
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2024-11-12 19:14:32 +0300

    Update adding_more_than_onelist.cpp
    some list eklendi

--- list's/adding_more_than_onelist.cpp
+++ list's/adding_more_than_onelist.cpp
@@ -1,3 +1,4 @@
+// as 2 different list
 #define _CRT_SECURE_NO_WARNINGS // Visual Studio kullanıyorsanız bu satır gereklidir.
 #include <stdio.h>
 #include <stdlib.h>
@@ -124,7 +125,7 @@ int main() {
 
 	for (int i = 0; i < 3; i++)
 	{
-		insert_item(name2[i], &first2);
+		insert_item(name2[i], &first2);//farklı bir first e eklersin
 	}
 	
 	printf("2. liste:\n");
@@ -134,5 +135,140 @@ int main() {
 	//printf("\nçıkartılmış yer\n");
 	//char name2[100][100] = { "asel" };
 	//delete_item(name2[0], &first);
+	return 0;
+}
+
+//as one list
+#define _CRT_SECURE_NO_WARNINGS // Visual Studio kullanıyorsanız bu satır gereklidir.
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX_LIST 10
+#define TRUE 1
+#define FALSE 0
+#define EMPTY -1
+
+typedef struct {
+	char name[10];
+	int link;
+} item;
+
+item linkedlist[MAX_LIST];
+int FREE;
+int first;
+int first2;
+
+void empty_list_maker() {
+	for (int i = 0; i < MAX_LIST; i++) {
+		linkedlist[i].link = i + 1;
+	}
+	linkedlist[MAX_LIST - 1].link = EMPTY;
+	FREE = 0;
+	first = EMPTY;
+	first2 = EMPTY;
+}
+
+int get_item(int* r) {
+	if (FREE == EMPTY) {
+		return FALSE;
+	} else {
+		*r = FREE;
+		FREE = linkedlist[FREE].link;
+		return TRUE;
+	}
+}
+
+void return_item(int r) {
+	linkedlist[r].link = FREE;
+	FREE = r;
+}
+
+void insert_item(char name[], int* list) {
+	int r, q, p;
+	if (get_item(&r)) {
+		strcpy(linkedlist[r].name, name);
+		q = EMPTY;
+		p = *list;
+
+		while (p != EMPTY && strcmp(linkedlist[p].name, name) < 0) {
+			q = p;
+			p = linkedlist[p].link;
+		}
+
+		if (q == EMPTY) {
+			*list = r;
+			linkedlist[r].link = p;
+		} else {
+			linkedlist[q].link = r;
+			linkedlist[r].link = p;
+		}
+	} else {
+		printf("\nNot enough free space!!");
+	}
+}
+
+void delete_item(char name[], int* list) {
+	int q, p;
+	q = EMPTY;
+	p = *list;
+	int l;
+
+	while (p != EMPTY && (l = strcmp(linkedlist[p].name, name)) < 0) {
+		q = p;
+		p = linkedlist[p].link;
+	}
+
+	if (p == EMPTY || l > 0) {
+		printf("\n%s cannot be found!!", name);
+	} else if (q == EMPTY) {
+		*list = linkedlist[p].link;
+		return_item(p);
+	} else {
+		linkedlist[q].link = linkedlist[p].link;
+		return_item(p);
+	}
+}
+
+void print_list(int list_start, const char* list_name) {
+	printf("\n%s - Only filled elements:\n", list_name);
+	int sayaç = list_start;
+	while (sayaç != EMPTY) {
+		printf("index\tname\tlink\n");
+		printf("%d\t%s\t%d\n", sayaç, linkedlist[sayaç].name, linkedlist[sayaç].link);
+		sayaç = linkedlist[sayaç].link;
+	}
+}
+
+void print() {
+	printf("\nFull array:\n");
+	for (int i = 0; i < MAX_LIST; i++) {
+		printf("index\tname\tlink\n");
+		printf("%d\t%s\t%d\n", i, linkedlist[i].name, linkedlist[i].link);
+	}
+	printf("FREE: %d\n", FREE);
+	printf("First: %d\n", first);
+	printf("First2: %d\n", first2);
+}
+
+int main() {
+	empty_list_maker();
+
+	char name[5][10] = { "ala", "agra", "oli", "bil", "vil" };
+	for (int i = 0; i < 5; i++) {
+		insert_item(name[i], &first);
+	}
+
+	print();
+
+
+	char name2[3][10] = { "aina", "baan", "takye" };
+	for (int i = 0; i < 3; i++) {
+		insert_item(name2[i], &first);// tek farkı aynı first e eklemen gerekir
+	}
+
+	print();
+
 	return 0;
 }
