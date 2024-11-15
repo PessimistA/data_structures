386f2bbb97bceb0886ee2674666bde530e1f3b8d
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2024-11-15 22:02:45 +0300

    Update prefix_to_postfix.cpp

--- stack_queue/prefix_to_postfix.cpp
+++ stack_queue/prefix_to_postfix.cpp
@@ -80,3 +80,92 @@ int main() {
 
     return 0;
 }
+
+//ver2 reverse kullanılmış
+#define _CRT_SECURE_NO_WARNINGS // Visual Studio kullanıyorsanız bu satır gereklidir.
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+char stack[MAX][MAX];
+int top = -1;
+
+void push(char* value) {
+	top++;
+	strcpy(stack[top],value) ;
+}
+void pop(char* op) {
+	if (top==-1)
+	{
+		return;
+	}
+	strcpy(op, stack[top]);
+	top--;
+}
+
+int isoperand(char ch) {
+	return isalpha(ch) || isdigit(ch);
+}
+void reverse(char* value) {
+	int len = strlen(value);
+	int start = 0;
+	while (!(start > len)) {
+		char geç = value[len-1];
+		value[len-1] = value[start];
+		value[start] = geç;
+		start++;
+		len--;
+	}
+}
+void reverseParanteheses(char* value) {
+	reverse(value);
+	int len = strlen(value);
+	for (int i = 0; i < len; i++)
+	{
+		if (value[i] == '(') {
+			value[i] = ')';
+		}
+		else if (value[i] == ')') {
+			value[i] = '(';
+		}
+	}
+}
+void translater(char value[],char prefix[]) {
+	int len = strlen(value);
+	int j = 0;
+	for (int i = 0; i < len; i++)
+	{
+		char ch = value[i];
+		if (isoperand(ch))
+		{
+			char operand[2] = {ch,'\0'};
+			push(operand);
+		}
+		else
+		{
+			char op1[MAX], op2[MAX];
+			pop(op1);
+			pop(op2);
+
+			char newep[MAX]="";
+			
+			strcat(newep, op1);
+			strcat(newep, op2);
+			strncat(newep, &ch, 1);
+			push(newep);
+		}
+	}
+	strcpy(prefix, stack[top]);
+}
+
+int main() {
+	char value[MAX] = "*+AB-CD";
+	char prefix[MAX];
+	reverseParanteheses(value);
+	printf("%s\n", value);
+	translater(value, prefix);
+	printf("%s", prefix);
+	return 0;
+}
