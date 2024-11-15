9535d80d8733eefd2ca4ec9b6e6d4280412f5fb6
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2024-11-16 00:06:01 +0300

    Update sınava_çalışma.cpp

--- data_structures/sınava_çalışma.cpp
+++ data_structures/sınava_çalışma.cpp
@@ -1327,3 +1327,137 @@ int main() {
 
     return 0;
 }
+
+//infix calculate
+#define _CRT_SECURE_NO_WARNINGS // Visual Studio kullanıyorsanız bu satır gereklidir.
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+int stack_number[MAX];
+int top_number = -1;
+char stack_opeartor[MAX];
+int top_operator = -1;
+
+void push_number(int value) {
+	top_number++;
+	stack_number[top_number]=value ;
+}
+void pop_number(int* op) {
+	if (top_number==-1)
+	{
+		return;
+	}
+	*op = stack_number[top_number];
+	top_number--;
+}
+void push_operator(char ch) {
+	top_operator++;
+	stack_opeartor[top_operator] = ch;
+}
+void pop_operator(char* op) {
+	if (top_operator == -1)
+	{
+		return;
+	}
+	*op = stack_opeartor[top_operator];
+	top_operator--;
+}
+
+int isoperand(char ch) {
+	return  isdigit(ch);
+}
+
+int isoperator(char ch) {
+	return ch=='+' || ch == '-' || ch == '*' || ch == '/' ;
+}
+int calculate(char ch,int op1,int op2) {
+	switch (ch)
+	{
+	case '+':
+		return op1 + op2;
+	case '-':
+		return op1 - op2;
+	case '*':
+		return op1 * op2;
+	case '/':
+		return op1 / op2;
+	default:
+		return -1;
+		break;
+	}
+}
+int precedence(char ch) {
+	switch (ch)
+	{
+	case '+':
+	case '-':
+		return 1;
+	case '*':
+	case '/':
+		return 2;
+	default:
+		return -1;
+		break;
+	}
+}
+void translater(char value[]) {
+	int len = strlen(value);
+	int j = 0;
+	int op1, op2;
+	char op;
+	for (int i = 0; i < len; i++)
+	{
+		char ch = value[i];
+		if (isoperand(ch))
+		{
+			push_number(ch - '0');
+		}
+		else if(ch == '(')
+		{
+			push_operator(ch);
+		}
+		else if (ch==')') {
+			while (top_operator!=-1 && stack_opeartor[top_operator] !='(') {
+				pop_operator(&op);
+				pop_number(&op1);
+				pop_number(&op2);
+				int answer= calculate(op,op2,op1);
+				push_number(answer);
+			}
+			pop_operator(&op);
+		}
+		else if(isoperator(ch))
+		{
+			while (top_operator != -1 && precedence(stack_opeartor[top_operator]) >= precedence(ch)) {
+				pop_operator(&op);
+				pop_number(&op1);
+				pop_number(&op2);
+				int answer = calculate(op, op2, op1);
+				push_number(answer);
+			}
+			push_operator(ch);
+		}
+	}
+	while (top_operator != -1) {
+		pop_operator(&op);
+		pop_number(&op1);
+		pop_number(&op2);
+		int answer = calculate(op, op2, op1);
+		push_number(answer);
+	}
+}
+
+int main() {
+	char value[MAX] = "2*(5*(3+6))/9-2";
+	char prefix[MAX];
+	//reverseParanteheses(value);
+	//printf("%s\n", value);
+	translater(value);
+	printf("%d", stack_number[top_number]);
+	return 0;
+}
+
+
