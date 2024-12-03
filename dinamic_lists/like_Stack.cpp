a6ad393ca01c6a83865ea5f38709618c8d5b3749
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2024-12-03 19:07:03 +0300

    Create like_Stack.cpp

---
+++ dinamic_lists/like_Stack.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int number;
+    struct node* next;
+};
+
+void print_list(struct node* head) {
+    struct node* current = head;
+    printf("List: ");
+    while (current != NULL) {
+        printf("%d  ", current->number);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+void insert_at_position(struct node** head, int item) {
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->number = item;//elemanı atar temp şu anda bulunduğumuz düğümü gösterir
+    temp->next = NULL;
+    temp->next = *head;
+    *head = temp;
+}
+void Delete(struct node** head) {
+	(*head) = (*head)->next;
+}
+
+void free_list(struct node** head) {
+    struct node* temp;
+    while (*head != NULL) {
+        temp = *head;
+        *head = (*head)->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct node* head = NULL;
+
+    // Eleman ekleme
+    insert_at_position(&head,  10);
+    insert_at_position(&head,  20); //önce eklenir ama sonra 30 gelince sağa kayar sonra 40 gelince en son sırada olur
+    insert_at_position(&head,  30);
+    insert_at_position(&head,  70);
+
+    // Listeyi yazdır
+    print_list(head);
+    Delete(&head);
+    print_list(head);
+    // Belleği temizle
+    free_list(&head);
+
+    return 0;
+}
