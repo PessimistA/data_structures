b0660914773943cc9e8349b790075279d97eac02
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2024-12-15 00:48:04 +0300

    Update balanced_parantheses.cpp

--- stack_queue/balanced_parantheses.cpp
+++ stack_queue/balanced_parantheses.cpp
@@ -53,3 +53,58 @@ int main() {
 
 	return 0;
 }
+//ver2
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+char stack[100];
+int top =-1;
+
+void push(char item) {
+	stack[++top] = item;
+}
+
+void pop() {
+	top--;
+}
+
+void print2() {
+	int k = top;
+	for (int i = 0; i < top; i++)
+	{
+		printf("%c->", stack[i]);
+	}
+	printf("\n");
+}
+int main() {
+
+	char dizi[] = "{[[elis]]a}()";
+	int length1= strlen(dizi);
+	printf("%d\n", length1);
+	for (int i = 0; i <length1; i++)
+	{
+		if (dizi[i]=='['|| dizi[i] == '(' || dizi[i] == '{' ) {
+			push(dizi[i]);
+		}
+		else if (dizi[i] == ']' || dizi[i] == ')' || dizi[i] == '}')
+		{
+			if (((dizi[i] == ']' && stack[top] == '[') ||
+				(dizi[i] == ')' && stack[top] == '(') ||
+				(dizi[i] == '}' && stack[top] == '{')))
+			{
+				pop();
+			}
+		}
+	}
+	print2();
+	if (top ==-1)
+	{
+		printf("dengeli");
+	}
+	else
+	{
+		printf("durum kotu");
+	}
+
+	return 0;
