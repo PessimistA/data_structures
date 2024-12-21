e7011c05c4c2f82e4126ae36cae9b031e42ab173
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2024-12-21 19:22:28 +0300

    Update reverse_stack.cpp

--- stack_queue/reverse_stack.cpp
+++ stack_queue/reverse_stack.cpp
@@ -46,3 +46,54 @@ int main() {
 	printf("%s", kelimeson);
 	return 0;
 }
+
+//ver2
+#include <stdio.h>
+#define MAX_STACK_SIZE 100
+
+
+void push(int stack[], int* top, int item) {
+	if (*top >= MAX_STACK_SIZE - 1) {
+		return;
+	}
+	else
+	{
+		stack[++(*top)] = item;
+	}
+}
+int pop(int stack[], int* top)
+{
+	if (*top == -1) {
+		return -1; //empty
+	}
+	return stack[(*top)--];
+}
+void reverse(int stack[], int* top, int stack2[], int* top2) {
+	while (*top != -1) {
+		push(stack2, top2, pop(stack, top));
+	}
+}
+void print(int stack[], int* top) {
+	printf("\nstack: ");
+	for (int i = 0; i <= *top; i++)
+	{
+		printf(" %d ", stack[i]);
+	}
+	printf("\n");
+}
+
+int main() {
+	int top = -1;
+	int top2 = -1;
+	int stack[MAX_STACK_SIZE];
+	int stack2[MAX_STACK_SIZE];
+	push(stack,&top,1);
+	push(stack, &top, 2);
+	push(stack, &top, 3);
+	push(stack, &top, 4);
+	push(stack, &top, 5);
+	print(stack,&top);
+	reverse(stack, &top, stack2, &top2);
+	print(stack2, &top2);
+	return 0;
+}
